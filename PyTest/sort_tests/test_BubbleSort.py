70ee6b2be9c73091554c66b9ca53a24ad8100218
ForrestSwift <78871922+ForrestSwift@users.noreply.github.com>
2021-11-14 22:11:44 -0500

    Updated formatting

--- PyTest/sort_tests/test_BubbleSort.py
+++ PyTest/sort_tests/test_BubbleSort.py
@@ -1,21 +1,22 @@
-
 # Bubble Sort
 def bubbleSort(arr):
-  """
+    """
   :param arr: an array to be sorted
   :returns: nothing
   """
-  n = len(arr)
-  # Traverse through all array elements
-  for i in range(n-1):
-    # range(n) also work but outer loop will repeat one time more than needed.
-    # Last i elements are already in place
-    for j in range(0, n-i-1):
-      # traverse the array from 0 to n-i-1
-      # Swap if the element found is greater
-      # than the next element
-      if arr[j] > arr[j + 1] :
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # range(n) also work but outer loop will repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
 # Driver code to test above
 
 # pytest method to check if bubble sort method sorts correctly
@@ -28,7 +29,7 @@ def test_sortsCorrectly():
     testarr = [9, 22, 31, 42, 54, 60, 80]
     assert arr == testarr
 
-#print ("Sorted array is:")
-#for i in range(len(arr)):
-#  print ("% d" % arr[i]),
 
+# print ("Sorted array is:")
+# for i in range(len(arr)):
+#  print ("% d" % arr[i]),
