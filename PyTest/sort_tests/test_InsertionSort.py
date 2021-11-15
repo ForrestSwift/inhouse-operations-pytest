fee55ab0565a316fb3f2110ce2e13094905b028b
ForrestSwift <78871922+ForrestSwift@users.noreply.github.com>
2021-11-14 22:12:10 -0500

    Reformatted

--- PyTest/sort_tests/test_InsertionSort.py
+++ PyTest/sort_tests/test_InsertionSort.py
@@ -1,22 +1,23 @@
 # Insertion sort
 def insertionSort(arr):
-  """
+    """
   :param arr: an array to be sorted
   :returns: nothing
   """
-  # Traverse through 1 to len(arr)
-  for i in range(1, len(arr)):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
 
-    key = arr[i]
+        key = arr[i]
 
-    # Move elements of arr[0..i-1], that are
-    # greater than key, to one position ahead
-    # of their current position
-    j = i-1
-    while j >=0 and key < arr[j] :
-        arr[j+1] = arr[j]
-        j -= 1
-    arr[j+1] = key
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
 
 # pytest method to check if insertion sort method sorts correctly
 def test_sortsCorrectly():
@@ -26,7 +27,6 @@ def test_sortsCorrectly():
     assert arr == testarr
 
 
-
-#print ("Sorted array is:")
-#for i in range(len(arr)):
+# print ("Sorted array is:")
+# for i in range(len(arr)):
 #  print ("%d" %arr[i])
