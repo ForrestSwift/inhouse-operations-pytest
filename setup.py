fbd92af27706dd4f18024b1add49298a1f3ced7d
samuelmmorse <78934300+samuelmmorse@users.noreply.github.com>
2021-11-21 22:29:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-      name='sort_lib',
-      version='1.0',
-      py_modules=['sort_lib.int_sort'],
+    name='sort_lib',
+    version='1.0',
+    py_modules=['sort_lib.int_sort'],
 )
