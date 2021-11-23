9b8de4dc24f6d0ca0a735042de410235c6be9c10
samuelmmorse <samuel.morse@maine.edu>
2021-11-22 22:20:08 -0500

    added decorators to run tests 100 times

--- test/test_randomSortTests.py
+++ test/test_randomSortTests.py
@@ -1,9 +1,10 @@
 from numpy import random
+import pytest
 from sort_lib.int_sort import bubble
 from sort_lib.int_sort import quick
 from sort_lib.int_sort import insertion
 
-
+@pytest.mark.repeat(100)
 def test_bubble():
     n = random.randint(100)
     i = random.randint(2147483646, size=(n))
@@ -12,7 +13,7 @@ def test_bubble():
         print(f"Given: {i}")
         assert a[k] <= a[k + 1]
 
-
+@pytest.mark.repeat(100)
 def test_insertion():
     n = random.randint(100)
     i = random.randint(2147483646, size=(n))
@@ -21,7 +22,7 @@ def test_insertion():
         print(f"Given: {i}")
         assert a[k] <= a[k + 1]
 
-
+@pytest.mark.repeat(100)
 def test_quick():
     n = random.randint(100)
     i = random.randint(2147483646, size=(n))
